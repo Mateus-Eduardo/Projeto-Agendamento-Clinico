37877f205dfa946b02a5492d4387c95e8c85845a
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-02 16:13:17 -0300

    create components

--- front/AgendamentoClinico-App/src/app/app.module.ts
+++ front/AgendamentoClinico-App/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PainelComponent } from './pages/painel/painel.component';
+import { AgendamentoComponent } from './pages/agendamento/agendamento.component';
+import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import { ConsultasComponent } from './pages/consultas/consultas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelComponent,
+    AgendamentoComponent,
+    PacientesComponent,
+    ConsultasComponent
   ],
   imports: [
     BrowserModule,
