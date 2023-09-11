9cfc3264bcd47a1abe52c1c7855fb66a2c82cb53
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-11 17:40:46 -0300

    config routing

--- front/AgendamentoClinico-App/src/app/app-routing.module.ts
+++ front/AgendamentoClinico-App/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'painel', loadChildren: () =>import('./pages/painel/painel.component').then(m => m.PainelComponent)},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-  
- }
+export class AppRoutingModule { }
