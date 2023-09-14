116e9b68fc24fc09392adb5790aacf9838f8c60a
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-14 16:35:29 -0300

    Update painel.component.ts

--- front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
+++ front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
 
 export class PainelComponent {
 
+  //api number: 3d62d08596112700906895c4fe29aeda
+
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds`;
 
 
 
