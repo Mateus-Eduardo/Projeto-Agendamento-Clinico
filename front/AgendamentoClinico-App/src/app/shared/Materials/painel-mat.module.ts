c7664f95dfa09faafc1cab6be5282f8db0cabb7e
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-14 16:35:31 -0300

    Update painel-mat.module.ts

--- front/AgendamentoClinico-App/src/app/shared/Materials/painel-mat.module.ts
+++ front/AgendamentoClinico-App/src/app/shared/Materials/painel-mat.module.ts
@@ -6,6 +6,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 
@@ -18,7 +21,11 @@ import { MatTableModule } from '@angular/material/table';
     MatTooltipModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatDividerModule
+
   ],
   declarations:[],
   providers:[],
