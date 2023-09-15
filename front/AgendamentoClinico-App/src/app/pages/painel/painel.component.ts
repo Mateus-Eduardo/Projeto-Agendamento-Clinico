06283d1fa500ac1be16fac74a7e0bb9288da0e71
Felipe Arruda <flphlk@gmail.com>
2023-09-15 20:42:18 -0300

    changing painel component

--- front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
+++ front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
@@ -1,9 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+        )
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms',
+          style({opacity: 0})
+        )
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms',
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}),
+            style({transform: 'rotate(2turn)', offset: '1'})
+          ])
+        )
+      ])
+    ])
+  ]
 })
 
 export class PainelComponent  {
