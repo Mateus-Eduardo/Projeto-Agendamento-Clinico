aef9d5a27160cb0eb520daf567245af6f4a2c892
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-19 13:26:11 -0300

    Update app-routing.module.ts

--- front/AgendamentoClinico-App/src/app/app-routing.module.ts
+++ front/AgendamentoClinico-App/src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'painel', pathMatch: 'full' },
   { path: 'painel', loadChildren: () =>import('./pages/painel/painel.module').then(m => m.PainelModule)},
   { path: 'agendamento', loadChildren: () =>import('./pages/agendamento/agendamento.module').then(m => m.AgendamentoModule)},
   { path: 'consultas', loadChildren: () =>import('./pages/consultas/consultas.module').then(m => m.ConsultasModule)},
   { path: 'pacientes', loadChildren: () =>import('./pages/pacientes/pacientes.module').then(m => m.PacientesModule)},
   { path: 'painel', loadChildren: () =>import('./pages/painel/painel.module').then(m => m.PainelModule)},
-
-
-
 ];
 
 @NgModule({
