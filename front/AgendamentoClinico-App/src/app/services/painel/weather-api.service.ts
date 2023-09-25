e8bf829e74c9ff31eb7db11c31ae2e97702a0568
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-25 18:25:53 -0300

    Update weather-api.service.ts

--- front/AgendamentoClinico-App/src/app/services/painel/weather-api.service.ts
+++ front/AgendamentoClinico-App/src/app/services/painel/weather-api.service.ts
@@ -7,17 +7,14 @@ import { environment } from 'src/environments/environment';
 })
 export class WeatherApiService {
 
-  apiKey = '3d62d08596112700906895c4fe29aeda'
-  URI: string = '';
+  URI: string = 'https://api.openweathermap.org/data/2.5/weather';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`
-  }
-
-  getWeather(cityName: string){
-    return this.http.get(`${this.URI}${cityName}`);
-
+  getWeather(cityName: string) {
+    const apiKey = '3d62d08596112700906895c4fe29aeda';
+    const params = { q: cityName, appid: apiKey };
+    return this.http.get(this.URI, { params });
   }
 
 
