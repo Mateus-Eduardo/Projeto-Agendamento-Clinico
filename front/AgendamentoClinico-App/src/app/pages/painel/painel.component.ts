0c818d34da2037e6d7974569c92f89699adff1bb
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-27 23:27:40 -0300

    Update painel.component.ts

--- front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
+++ front/AgendamentoClinico-App/src/app/pages/painel/painel.component.ts
@@ -2,6 +2,7 @@ import { animate, keyframes, style, transition, trigger } from '@angular/animati
 import { Component, OnInit, } from '@angular/core';
 import { WeatherApiService } from 'src/app/services/painel/weather-api.service';
 
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -10,13 +11,13 @@ import { WeatherApiService } from 'src/app/services/painel/weather-api.service';
     trigger('fadeInOut', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('350ms',
-          style({ opacity: 1 })
+        animate('1000ms',
+          style({ opacity: 2 })
         )
       ]),
       transition(':leave', [
-        style({ opacity: 1 }),
-        animate('350ms',
+        style({ opacity: 2 }),
+        animate('1000ms',
           style({ opacity: 0 })
         )
       ])
@@ -37,11 +38,46 @@ import { WeatherApiService } from 'src/app/services/painel/weather-api.service';
 export class PainelComponent implements OnInit {
   weather: any;
   cityName: string = "Barra Bonita, São Paulo";
+  greetings: string = '';
+  iconMapping: { [key: string]: string } = {
+    '01d': 'clear-sky.png',
+    '02d': 'few-clouds.png',
+    '03d': 'scattered-clouds.png',
+    '04d': 'broken-clouds.png',
+    '09d': 'shower-rain.png',
+    '10d': 'rain.png',
+    '11d': 'thunderstorm.png',
+    '13d': 'snow.png',
+    '50d': 'mist.png',
+    '01n': 'clear-sky-night.png',
+    '02n': 'few-clouds-night.png',
+    '03n': 'scattered-clouds-night.png',
+    '04n': 'broken-clouds-night.png',
+    '09n': 'shower-rain-night.png',
+    '10n': 'rain-night.png',
+    '11n': 'thunderstorm-night.png',
+    '13n': 'snow-night.png',
+    '50n': 'mist-night.png',
+  };
 
-  constructor(private weatherService: WeatherApiService) { }
+  constructor(private weatherService: WeatherApiService) {
+    this.setGreetings();
+  }
 
   ngOnInit(): void {
     this.getWeather(this.cityName);
+  }
+
+  setGreetings() {
+    const horaAtual = new Date().getHours();
+
+    if (horaAtual >= 0 && horaAtual < 12) {
+      this.greetings = 'Bom Dia';
+    } else if (horaAtual >= 12 && horaAtual < 18) {
+      this.greetings = 'Boa Tarde';
+    } else {
+      this.greetings = 'Boa Noite';
+    }
   }
 
   getWeather(cityName: string) {
@@ -49,20 +85,22 @@ export class PainelComponent implements OnInit {
       (res: any) => {
         console.log(res);
 
-
+        // Mapeie os dados da API para o objeto 'weather' de forma adequada
         const kelvinTemp = res.main.temp;
-        const celsiusTemp = kelvinTemp - 273.15;
+        const celsiusTemp = kelvinTemp - 274.15;
 
         const kelvinTempMin = res.main.temp_min;
-        const celsiusTempMin = kelvinTempMin - 273.15;
+        const celsiusTempMin = kelvinTempMin - 277.15;
 
         const kelvinTempMax = res.main.temp_max;
         const celsiusTempMax = kelvinTempMax - 273.15;
 
         const KelvinFeelsLike = res.main.feels_like;
-        const CelsiusTempFellsLike = KelvinFeelsLike - 273.15;
+        const CelsiusTempFellsLike = KelvinFeelsLike - 272.15;
 
-        console.log("Valor em Celsius (após a conversão):", celsiusTemp);
+        // Mapeie outros dados da API conforme necessário
+        const iconCode = res.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
         this.weather = {
           ...res,
@@ -71,8 +109,12 @@ export class PainelComponent implements OnInit {
             temp: celsiusTemp,
             temp_min: celsiusTempMin,
             temp_max: celsiusTempMax,
-            feels_like: CelsiusTempFellsLike
-          }
+            feels_like: CelsiusTempFellsLike,
+          },
+          weather: {
+            ...res.weather[0],
+            iconUrl: iconUrl,
+          },
         };
       },
       (err) => {
@@ -88,3 +130,7 @@ export class PainelComponent implements OnInit {
     return false;
   }
 }
+
+
+
+
