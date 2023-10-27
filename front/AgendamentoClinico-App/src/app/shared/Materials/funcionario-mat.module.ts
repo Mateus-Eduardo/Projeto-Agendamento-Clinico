7758d1a8f51c57f36525faab3822ab8a0aa1c1bb
Mateus-Eduardo <mateus.silverio2022@gmail.com>
2023-10-27 15:13:18 -0300

    Update funcionario-mat.module.ts

--- front/AgendamentoClinico-App/src/app/shared/Materials/funcionario-mat.module.ts
+++ front/AgendamentoClinico-App/src/app/shared/Materials/funcionario-mat.module.ts
@@ -3,18 +3,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card'; // Importe MatCardModule
 import { MatProgressBarModule } from '@angular/material/progress-bar'; // Importe MatProgressBarModule
 import { MatDividerModule } from '@angular/material/divider'; // Importe MatDividerModule
-import { MatInputModule } from '@angular/material/input'; // Importe MatInputModule
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -26,7 +20,6 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatButtonModule,
     MatButtonModule,
     MatTooltipModule,
-    MatInputModule,
     MatFormFieldModule,
     MatTableModule,
     MatProgressBarModule,
