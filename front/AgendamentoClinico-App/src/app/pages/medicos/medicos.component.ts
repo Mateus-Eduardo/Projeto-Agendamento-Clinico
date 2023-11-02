c7717750e630d1bfb425a8fee6f794ff3dc7ed62
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-11-02 18:28:31 -0300

    update component medicos

--- front/AgendamentoClinico-App/src/app/pages/medicos/medicos.component.ts
+++ front/AgendamentoClinico-App/src/app/pages/medicos/medicos.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { DATA_PEOPLE } from './model/data-people';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-medicos',
@@ -6,5 +11,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./medicos.component.scss']
 })
 export class MedicosComponent {
+
+  public dataSource = new MatTableDataSource(DATA_PEOPLE);
+
+  public displayColumns: string[] = ['name', 'cpf', 'address', 'phone', 'city', 'actions'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  editUser(id: number) {
+    //esperando o backend
+  }
+
+  deleteUser(id: number) {
+    //esperando o backend
+  }
 
 }
--- front/AgendamentoClinico-App/src/app/shared/Materials/medicos-mat.modules.ts
+++ front/AgendamentoClinico-App/src/app/shared/Materials/medicos-mat.modules.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 
 
@@ -18,7 +19,8 @@ import { MatTableModule } from '@angular/material/table';
     MatTooltipModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ],
   declarations:[],
   providers:[],
