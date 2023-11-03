d3e94a1a737bf356f61247335a932a33c7d64e36
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-11-03 01:24:35 -0300

    config navbar

--- front/AgendamentoClinico-App/src/app/shared/components/navbar/navbar.component.ts
+++ front/AgendamentoClinico-App/src/app/shared/components/navbar/navbar.component.ts
@@ -1,16 +1,80 @@
-import { Input, Component } from '@angular/core';
+import { Component, HostListener, EventEmitter, Output, Input } from '@angular/core';
 import { MenuItem } from '../../models/menuItem';
 import { menuItems } from '../../models/menu';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms',
+          style({opacity: 1})
+        )
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms',
+          style({opacity: 0})
+        )
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms',
+          keyframes([
+            style({transform: 'rotate(0deg)', offset: '0'}),
+            style({transform: 'rotate(2turn)', offset: '1'})
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class NavbarComponent {
 
-  @Input() item!: MenuItem;
-  items_menu: MenuItem[] = menuItems;  
+  collapsed = false;
+  noCollapsed = true
+  screenWidth = 0;
+  items_menu: MenuItem[] = menuItems;
+  onToggleSideNav: any;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768 ) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+
+  ngOnInit(): void {
+      this.screenWidth = window.innerWidth;
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  mouseenter() {
+    if (!this.collapsed) {
+      this.collapsed = true;
+    }
+  }
+
+  mouseleave() {
+    if (this.collapsed) {
+      this.collapsed = false;
+    }
+  }
 }
