747d636e947a7810231fe731f3e2e2ffc1a52cf9
Felipe-Hlk <felipearrudappereira@gmail.com>
2023-11-15 14:42:56 -0300

    Update paciente.routes.js

--- back/agendamento-api/src/routes/paciente.routes.js
+++ back/agendamento-api/src/routes/paciente.routes.js
@@ -11,11 +11,11 @@ router.post('/pacientes', pacienteController.createPaciente);
 // => Rota responsável por listar todos os pacientes: (GET): localhost:3000/api/pacientes
 router.get('/pacientes', pacienteController.listAllPacientes);
 
-// // = > Rota responsável por listar um pacientes por Id: (GET): localhost:3000/api/pacientes/Id
-// router.get('/pacientes/:id', pacienteController.findPacienteById);
+// = > Rota responsável por listar um pacientes por Id: (GET): localhost:3000/api/pacientes/Id
+router.get('/pacientes/:id', pacienteController.findPacienteById);
 
-// // = > Rota responsável por atualizar um determinado pacientes por Id: (PUT): localhost:3000/api/pacientes/Id
-// router.put('/pacientes/:id', pacienteController.UpdatePacienteById);
+// = > Rota responsável por atualizar um determinado pacientes por Id: (PUT): localhost:3000/api/pacientes/Id
+router.put('/pacientes/:id', pacienteController.UpdatePacienteById);
 
 // // = > Rota responsável por deletar/excluir um determinado pacientes por Id: (PUT): localhost:3000/api/pacientes/Id
 // router.delete('/pacientes/:id', pacienteController.deletePacienteById);
