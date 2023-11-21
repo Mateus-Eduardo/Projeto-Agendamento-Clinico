912b9a13475609f4cc45cb844d6d12bb4e9eebf5
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-11-21 18:09:39 -0300

    Update pacientes.service.ts

--- front/AgendamentoClinico-App/src/app/services/pacientes/pacientes.service.ts
+++ front/AgendamentoClinico-App/src/app/services/pacientes/pacientes.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import Paciente from 'src/app/classes/pacientes/Paciente';
+
 
 
 @Injectable({
@@ -12,17 +14,28 @@ export class PacientesService {
 
   constructor(private http: HttpClient) { }
 
-  createNewPaciente(nome_paciente: string, cpf_paciente: number, telefone_paciente: number, celular_paciente: number, endereco_paciente: string, cidade_paciente: string) {
-      const paciente = {
-        nome_paciente,
-        cpf_paciente,
-        telefone_paciente,
-        celular_paciente,
-        endereco_paciente,
-        cidade_paciente
-      };
+  getPacientes(): Observable<Paciente[]> { // Declarando o retorno como um Observable
+    return this.http.get<Paciente[]>(`${this.uri}/pacientes`);
+  }
 
-      console.log(paciente);
+  createNewPaciente(
+    nome_paciente: string,
+    cpf_paciente: number,
+    telefone_paciente: number,
+    celular_paciente: number,
+    endereco_paciente: string,
+    cidade_paciente: string
+  ) {
+    const paciente = {
+      nome_paciente,
+      cpf_paciente,
+      telefone_paciente,
+      celular_paciente,
+      endereco_paciente,
+      cidade_paciente  
+    };
+
+    console.log(paciente);
 
       // (POST - URL no Back-End): http://localhost:3000/api/pacientes
     this.http.post(`${this.uri}/pacientes`, paciente)
