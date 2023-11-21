88fd7ba7abc3ef02b29920869e70a34b6e98ed73
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-11-20 23:51:38 -0300

    Create Paciente.ts

---
+++ front/AgendamentoClinico-App/src/app/pages/pacientes/model/Paciente.ts
@@ -0,0 +1,8 @@
+// export default class Paciente {
+//   nome_paciente: string;
+//   cpf_paciente: number;
+//   telefone_paciente: number;
+//   celular_paciente: number;
+//   endereco_paciente: string;
+//   cidade_paciente: string;
+// }
