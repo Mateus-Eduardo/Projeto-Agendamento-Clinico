2aec7b9f772f15900c1291428aba2ce6bfa70a72
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-11-22 23:37:40 -0300

    config component pacientes

--- front/AgendamentoClinico-App/src/app/pages/pacientes/cadastro-pacientes/cadastro-pacientes.component.ts
+++ front/AgendamentoClinico-App/src/app/pages/pacientes/cadastro-pacientes/cadastro-pacientes.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PacientesService } from '../../../services/pacientes/pacientes.service';
-
+import { MaskService } from '../../../services/mask/paciente-mask.service';
 
 @Component({
   selector: 'app-cadastro-pacientes',
@@ -11,43 +11,72 @@ import { PacientesService } from '../../../services/pacientes/pacientes.service'
 })
 export class CadastroPacientesComponent {
 
+  valorCPF!: string;
   cadastroPacienteForm!: FormGroup;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<CadastroPacientesComponent>,
-
     private formBuilder: FormBuilder,
-
-    public pacienteService: PacientesService
-
+    public pacienteService: PacientesService,
+    private maskService: MaskService
   ) {
     this.cadastroPacienteForm = data.cadastroPacienteForm;
     this.createForm();
   }
 
-  createNewPaciente(nome_paciente: string, cpf_paciente: number, telefone_paciente: number, celular_paciente: number, endereco_paciente: string, cidade_paciente: string){
+  onInputChange(event: any) {
+    const inputValue = event.target.value;
+    const formattedCPF = this.maskService.formatCPF(inputValue);
+    this.cadastroPacienteForm.patchValue({ cpf_paciente: formattedCPF }, { emitEvent: true});
+
+    // Desativar Validators.required se o CPF estiver completo
+    const cpfControl = this.cadastroPacienteForm.get('cpf_paciente');
+    if (cpfControl?.value && cpfControl.value.length === 11) {
+      cpfControl.clearValidators();
+    } else {
+      cpfControl?.setValidators([Validators.required, Validators.maxLength(11), Validators.pattern('^[0-9]*$')]);
+    }
+    cpfControl?.updateValueAndValidity();
+  }
+
+  createNewPaciente(nome_paciente: string, cpf_paciente: number, telefone_paciente: number, celular_paciente: number, endereco_paciente: string, cidade_paciente: string) {
     this.pacienteService.createNewPaciente(nome_paciente, cpf_paciente, telefone_paciente, celular_paciente, endereco_paciente, cidade_paciente)
   };
 
-  createForm(){
+  createForm() {
+
     this.cadastroPacienteForm = this.formBuilder.group({
       nome_paciente: ['', Validators.required],
-      cpf_paciente: ['', Validators.required],
-      telefone_paciente: ['',],
+      cpf_paciente:['', Validators.required],
+      telefone_paciente: [''],
       celular_paciente: ['', Validators.required],
       endereco_paciente: ['', Validators.required],
       cidade_paciente: ['', Validators.required],
-    })
+    });
 
   }
 
+  public resetForm(): void{
+    this.cadastroPacienteForm.reset();
+  }
+
+
+
+  // formatarCPF(value: number): void {
+  //   this.valorCPF = this.maskService.formatCPF(value);
+  // }
+
+
+
   ngOnInit(): void {
+
   }
 
   sair(): void {
     this.dialogRef.close();
   }
+
 
   // onSubmit() {
   //   if (this.cadastroPacienteForm.valid) {
