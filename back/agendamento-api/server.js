dfcc562e2549f90a48b8fc10ee0d21c4ff3fef1b
Felipe-Hlk <felipearrudappereira@gmail.com>
2023-11-27 22:00:02 -0300

    Update server.js

--- back/agendamento-api/server.js
+++ back/agendamento-api/server.js
@@ -8,6 +8,7 @@ app.listen(port, () => {
   console.log('Aplicação sendo executada na porta:', port);
   console.log(`Rota responsável por criar um novo Paciente: (Post): localhost:3000/api/pacientes`);
   console.log(`Rota para cadastrar Funcionario: localhost:3000/api/funcionarios`);
-  console.log(`Rota para login de  Funcionario: localhost:3000/api/auth/login`);
+  console.log(`Rota para login de Funcionario: localhost:3000/api/auth/login`);
+  console.log(`Rota para cadastrar novo medico: localhost:3000/api/medico`);
 
 });
