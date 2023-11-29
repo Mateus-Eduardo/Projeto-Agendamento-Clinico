d7c910ce2ab9248021ea2aba50f3742e6849f057
Mateus Eduardo Silvério <115047768+Mateus-Eduardo@users.noreply.github.com>
2023-11-29 20:37:40 -0300

    api-login

--- front/AgendamentoClinico-App/src/app/services/auth/auth.service.ts
+++ front/AgendamentoClinico-App/src/app/services/auth/auth.service.ts
@@ -1,26 +1,25 @@
-
+// auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import Auth from 'src/app/classes/auth/Auth';
-
+import { catchError, map } from 'rxjs/operators';
 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthService {
+  private isAuthenticatedValue: boolean = false;
+  private authToken: string = '';
 
-  uri = 'http://localhost:3000/api';
+  uri = 'http://localhost:3000/api/auth';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   loginFuncionario(
     email_funcionario: string,
-    senha_funcionario: string,
-  ) {
+    senha_funcionario: string
+  ): Observable<any> {
     const auth = {
       email_funcionario,
       senha_funcionario,
@@ -28,16 +27,37 @@ export class AuthService {
 
     console.log(auth);
 
+    return this.http.post(`${this.uri}/login`, auth).pipe(
+      catchError((error) => {
+        console.error('Erro ao logar o funcionário:', error);
+        console.error('Erro completo:', error.error);
+        throw error;
+      }),
+      map((response: any) => {
+        const token = response.token;
 
+        if (token) {
+          this.authToken = token;
+          this.setAuthenticated(true);
+        }
 
-    this.http.post(`${this.uri}/auth/login`, auth)
-      .pipe(
-        catchError(error => {
-          console.error('Erro ao logar o funcionário:', error);
-          console.error('Erro completo:', error.error);
-          throw error;
-        })
-      )
-      .subscribe((res: any) => console.log('Funcionario Logado'));
+        return response;
+      })
+    );
+  }
+
+  logout() {
+    // Lógica de logout, chamada à API, etc.
+    // Aqui, você pode adicionar qualquer lógica necessária para deslogar o usuário.
+    // Por exemplo, chamar uma API para invalidar o token de autenticação.
+    this.setAuthenticated(false);
+  }
+
+  isAuthenticated(): boolean {
+    return this.isAuthenticatedValue;
+  }
+
+  setAuthenticated(isAuthenticated: boolean) {
+    this.isAuthenticatedValue = isAuthenticated;
   }
 }
